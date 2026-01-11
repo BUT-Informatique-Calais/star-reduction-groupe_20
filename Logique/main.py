8b7eed4b274983aa62673145b7d74a12cabe01e6
LEVITRE Mathys <levitre.mathys30@gmail.com>
2026-01-11 17:43:50 +0100

    Remove image processing code from main.py

--- Logique/main.py
+++ Logique/main.py
@@ -1,5 +1,1 @@
-from reduce_stars import reduce_stars 
-from saveImage import saveImage 
 
-image = reduce_stars("./../examples/HorseHead.fits", fwhm=8.0, threshold=5.0, kernelSize=8, ksize=6, sigmaX=4) 
-saveImage(image, "pornoAnnee50", "./") 
